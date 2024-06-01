7819749cbd11372b341a68b0d6a1272bb9ea33c5
Mahmoud S Albohaisi <mohabohaisi@gmail.com>
2024-06-01 23:45:55 +0300

    fix list ul problem and reduce the size fo images

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 const barBtn = document.querySelector(".bars");
 const listLink = document.querySelector(".list-link");
 barBtn.addEventListener("click", function () {
-  if(listLink.classList.contains('show-btn')) {
-    listLink.classList.remove('show-btn');
+  if (listLink.style.display === 'none' || listLink.style.display === '') {
+    listLink.style.display = 'flex';
   } else {
-    listLink.classList.add('show-btn');
+    listLink.style.display = 'none';
   }
 });
 
-
 //carousel
-const myCarouselElement = document.querySelector('#myCarousel')
+const myCarouselElement = document.querySelector("#myCarousel");
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 1500,
 });
 
-
 //country select code
-$(document).ready(function() {
-  $('.js-example-basic-single').select2();
+$(document).ready(function () {
+  $(".js-example-basic-single").select2();
 });
